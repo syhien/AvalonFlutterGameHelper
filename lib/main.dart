b3c7178bbe2aaca76dffe0c1cdfc03f7bd60c977
syhien <syhien@outlook.at>
2022-01-24 20:56:18 +0800

    初版完成

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
-import 'package:http/http.dart';
 
 void main() => runApp(const AvalonApp());
 
@@ -11,7 +10,8 @@ class AvalonApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Avalon',
-      home: InputPage(),
+      home: const InputPage(),
+      theme: ThemeData(brightness: Brightness.dark, primaryColor: Colors.black),
     );
   }
 }
@@ -84,8 +84,104 @@ class _InputPageState extends State<InputPage> {
                 },
                 options:
                     Options(contentType: Headers.formUrlEncodedContentType));
-            print(response.data);
+            Map<String, dynamic> doc = response.data;
+            List<int> seenPlayers = doc['seenPlayers'].cast<int>();
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => IdentityPage(
+                        identity: doc['identity'], seenPlayers: seenPlayers)));
           }),
+    );
+  }
+}
+
+class IdentityPage extends StatelessWidget {
+  const IdentityPage(
+      {Key? key, required this.identity, required this.seenPlayers})
+      : super(key: key);
+
+  final String identity;
+  final List<int> seenPlayers;
+
+  static const Map<String, String> identitiesChineseMap = {
+    'Merlin': '🧙梅林🧙',
+    'Percival': '🔍派西维尔🔍',
+    'Loyal Servant of Arthur': '⛨亚瑟的忠臣⛨',
+    'Morgana': '🎭莫甘娜🎭',
+    'Assassin': '🔪刺客🔪',
+    'Oberon': '🤪奥伯伦🤪',
+    'Minion of Mordred': '👿莫德雷德的爪牙👿',
+    'Mordred': '😈莫德雷德😈'
+  };
+
+  static const Map<String, Color> identitiesColorMap = {
+    'Merlin': Colors.lightBlue,
+    'Percival': Colors.lightBlue,
+    'Loyal Servant of Arthur': Colors.lightBlue,
+    'Morgana': Colors.red,
+    'Assassin': Colors.red,
+    'Oberon': Colors.red,
+    'Minion of Mordred': Colors.red,
+    'Mordred': Colors.red,
+  };
+
+  static const Map<String, String> identitiesHintMap = {
+    'Merlin': ' - 把握场上好恶形势，适时传递必要的信息\n - 谨言慎行，隐藏身份',
+    'Percival': ' - 分辨梅林与莫甘娜，接受梅林的讯息并保护梅林不暴露\n - 适时亮明身份并参与任务，提高任务成功的可能性',
+    'Loyal Servant of Arthur': ' - 把握场上好恶形势，暗中辨识梅林并听从指引\n - 积极参与任务并掩护梅林身份',
+    'Morgana': ' - 模仿梅林欺骗派西维尔以获得信任，尽可能破坏任务\n - 注意场上其他人的言行举止，暗中辨识梅林',
+    'Assassin': ' - 积极参与任务并尽可能破坏任务\n - 注意场上其他人的言行举止，暗中辨识梅林',
+    'Oberon': ' - 积极参与任务并尽可能破坏任务\n - 注意场上其他人的言行举止，暗中辨识梅林\n - 暗中辨识友方角色并适当地协助他们',
+    'Minion of Mordred': ' - 积极参与任务并尽可能破坏任务\n - 注意场上其他人的言行举止，暗中辨识梅林',
+    'Mordred': ' - 注意场上其他人的言行举止，暗中辨识梅林\n - 作为“底牌”，在关键时刻破坏任务',
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('确认身份'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Center(
+                child: Text(
+                  identitiesChineseMap[identity]!,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 46, color: identitiesColorMap[identity]),
+                ),
+              )),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: Center(
+                child: Image.asset(
+              'images/' + identity + '.jpg',
+              width: 350,
+            )),
+          ),
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Row(
+                children: <Widget>[
+                  const Text('你看到了', style: TextStyle(fontSize: 20)),
+                  Text(seenPlayers.toString(),
+                      style: const TextStyle(fontSize: 40))
+                ],
+              )),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: Text(
+              identitiesHintMap[identity]!,
+              style: const TextStyle(fontSize: 25),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
